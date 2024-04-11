6036ef1f1955dbf7e66bc0ea3d35d78482a0a460
cavalcanteyury <yurypcfarias@gmail.com>
2024-04-11 17:50:09 -0300

    token test

--- token.rb
+++ token.rb
@@ -1,8 +1,58 @@
+require 'byebug'
+
 class Token
   attr_reader :type, :literal
 
-  def initialize(token_info = [])
-    (@type, @literal) = token_info
+  TOKEN_TYPE = {
+    "":        "EOF",
+    "ILLEGAL": "ILLEGAL",
+
+    # Identifiers + literals
+    "IDENT":   "IDENT",
+    "INT":     "INT",
+
+    # Operators
+    "=":       "ASSIGN",
+    "+":       "PLUS",
+
+    # Delimiters
+    ",":       "COMMA",
+    ";":       "SEMICOLON",
+    "(":       "LPAREN",
+    ")":       "RPAREN",
+    "{":       "LBRACE",
+    "}":       "RBRACE",
+  }.freeze
+
+  KEYWORDS = {
+    "let": "LET",
+    "fn":  "FUNCTION",
+  }.freeze
+
+  def initialize(literal = nil)
+    parse(literal) unless literal.nil?
+  end
+
+  def type=(t)
+    @type = t
+  end
+
+  def literal=(l)
+    @literal = l
+  end
+
+  def literal_to_type(t)
+    return @type = TOKEN_TYPE[t.to_sym] if TOKEN_TYPE.has_key?(t.to_sym)
+
+    return @type = KEYWORDS[t.to_sym] if KEYWORDS.has_key?(t.to_sym)
+
+    @type = t
+  end
+
+
+  def parse(literal)
+    self.type = literal_to_type(literal)
+    self.literal = literal
   end
 end
 
